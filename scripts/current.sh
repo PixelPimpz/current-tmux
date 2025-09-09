bb638645f434fc4dacedef4886c2452d4ff1d1df
lucifer-morningstar <slobeck@gmail.com>
2025-09-09 06:17:47 -0700

    fixd Current-style

--- scripts/current.sh
+++ scripts/current.sh
@@ -10,7 +10,7 @@ main () {
 
 setStatusBar () {
   tmux set -g @CurrentData "#W"
-  tmux set -g @Current-style "[fg=#{@Light},bg=#{@Dark2}]"
+  tmux set -g @Current-style "fg=#{@Light},bg=#{@Dark2}"
   tmux set -g @Current "#{E:@Current-style}#{@TriangleL}#[inverse] #W #[noinverse]#[bg=#{Dark0}]#{@TriangleRInverse}"
   tmux set -g status-right "#{E:@Current}#{T:@DateTime}"
   tmux set -g status-right-length 0
