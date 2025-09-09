66853cdc80d8c46cd13d38622146e1e556dea71c
lucifer-morningstar <slobeck@gmail.com>
2025-09-09 05:50:14 -0700

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+# Current 
+### a simple plugin for TMUX that provides #{@current} and provides the the name of the currently open document if the pane is running a process with open douments like (neo)VIM. If no documents are open, it gets the name of the currentlyt running process and, if the tmux-nerd-font-window-name plugin is also present, an application icon in the nerd font you're using.
+
+
---
+++ scripts/current.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+main () {
+  printf "%s is running, but WHY?\n" $0
+  SPID=$( tmux display -p "#{pane_pid}" )
+  CPID=$( pgrep -P $SPID -l )
+  MPID="$( ps -q $SPID -o comm= )"
+  setStatusBar
+  dbug
+}
+
+setStatusBar () {
+  tmux set -g @CurrentData "#W"
+  tmux set -g @Current-style "[fg=#{@Light},bg=#{@Dark2}]"
+  tmux set -g @Current "#{E:@Current-style}#{@TriangleL}#[inverse] #W #[noinverse]#[bg=#{Dark0}]#{@TriangleRInverse}"
+  tmux set -g status-right "#{E:@Current}#{T:@DateTime}"
+  tmux set -g status-right-length 0
+}
+
+dbug () {
+  echo "Shell PID: $SPID"
+  echo "Child PID: $CPID"
+  echo "InUse $MPID"
+}
+main
